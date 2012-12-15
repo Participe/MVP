a110d139184e8de4a53c89ba5adcdec2793f6202
asuvorov <artem.suvorov@gmail.com>
2012-12-15 13:29:21 +0200

    #18 terms and conditions accept text

--- participe/urls.py
+++ participe/urls.py
@@ -9,8 +9,13 @@ urlpatterns = patterns('',
     # Authentication
         
     # Home
-    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}, name='home'),
-    url(r'^home/$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}, name='home'),
+    url(r'^$', 'django.views.generic.simple.direct_to_template',
+            {'template': 'home.html'}, name='home'),
+    url(r'^home/$', 'django.views.generic.simple.direct_to_template',
+            {'template': 'home.html'}, name='home'),
+    url(r'^tnc/$', 'django.views.generic.simple.direct_to_template',
+            {'template': 'terms_and_conditions.html'},
+            name='terms_and_conditions'),
     
     # Take into account, that Avatar templates are overridden here
     url(r'^avatar/', include('avatar.urls')),
@@ -27,21 +32,25 @@ urlpatterns += patterns('participe.account.views',
     url(r'^accounts/signup/$', 'signup', name='signup'),
     url(r'^accounts/profile/$', 'view_profile', name='view_profile'),
     url(r'^accounts/edit/$', 'edit_profile', name='edit_profile'),
-    url(r'^accounts/password/reset/$', 'reset_password', name='reset_password'),
+    url(r'^accounts/password/reset/$', 'reset_password',
+            name='reset_password'),
     )
 
 urlpatterns += patterns('participe.challenge.views',
     # Challenge
     url(r'^challenges/$', 'challenge_list', name='challenge_list'),
     url(r'^challenges/create/$', 'challenge_create', name='challenge_create'),
-    url(r'^challenges/(?P<challenge_id>\d+)/$', 'challenge_detail', name='challenge_detail'),
+    url(r'^challenges/(?P<challenge_id>\d+)/$', 'challenge_detail',
+            name='challenge_detail'),
     )
 
 urlpatterns += patterns('participe.organization.views',
     # Organization
     url(r'^organizations/$', 'organization_list', name='organization_list'),
-    url(r'^organizations/create/$', 'organization_create', name='organization_create'),
-    url(r'^organizations/(?P<organization_id>\d+)/$', 'organization_detail', name='organization_detail'),
+    url(r'^organizations/create/$', 'organization_create',
+            name='organization_create'),
+    url(r'^organizations/(?P<organization_id>\d+)/$', 'organization_detail',
+            name='organization_detail'),
     )
 
 # The big, fat disclaimer
